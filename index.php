3c30450a5adedd0927b736bb220915c612d51b26
Veronica Celseti <veronicacelseti12@gmail.com>
2020-06-15 16:07:58 +0200

    esercizio complatato con funzione strlen

--- index.php
+++ index.php
@@ -4,15 +4,33 @@
 
 <?php
 
-    $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     Excepteur sint occaecat cupidatat non proident,
     sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-    $bad_words = $_GET ['word'];
-    $censored = str_replace($bad_words, '***', $testo);
-    echo $censored;
-
+//recupero la parola da censurare
+    $bad_words = $_GET['word'];
+//con la funzione str_replace sostituisco con tre asterischi tutte le occorrenze di quella parola nel testo
+    $censored = str_replace($bad_words, '***', $text);
+//stampo il testo censurato
+        // echo($censored);
+//con la funzione strlen calcolo la lunghezza della stringa
+    $string_length = strlen($censored);
+//stampo la lunghezza della stringa
+        // echo($string_length);
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Bad Words</title>
+    </head>
+    <body>
+        <p>
+            <?php echo $censored; echo $string_length; ?>
+        </p>
+    </body>
+</html>
